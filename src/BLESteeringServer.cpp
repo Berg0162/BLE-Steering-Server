231d29629c88c2cc212ea71de7eea3ac39923316
Berg0612 <57005514+Berg0162@users.noreply.github.com>
2025-05-03 10:56:30 +0200

    Created initial version

---
+++ src/BLESteeringServer.cpp
@@ -0,0 +1,217 @@
+#include "BLESteeringServer.h"
+
+// -------------------------------------------------------------------------------------------
+// COMPILER DIRECTIVE to allow/suppress DEBUG messages that help debugging...
+// Uncomment general "#define DEBUG" to activate for this class
+#define DEBUG
+// Include these debug utility macros in all cases!
+#include "DebugUtils.h"
+// -------------------------------------------------------------------------------------------
+
+// BLE Definitions
+#define STEERING_DEVICE_UUID      BLEUUID("347b0001-7635-408b-8918-8ff3949ce592")
+#define STEERING_ANGLE_CHAR_UUID  BLEUUID("347b0030-7635-408b-8918-8ff3949ce592")    // notify
+#define STEERING_RX_CHAR_UUID     BLEUUID("347b0031-7635-408b-8918-8ff3949ce592")    // write
+#define STEERING_TX_CHAR_UUID     BLEUUID("347b0032-7635-408b-8918-8ff3949ce592")    // indicate
+
+#define UUID16_SVC_BATTERY        BLEUUID((uint16_t)0x180F)
+#define UUID16_CHR_BATTERY_LEVEL  BLEUUID((uint16_t)0x2A19)
+
+// Steering defines
+#define MAX_STEER_ANGLE (35.0F)
+#define STEERANGLETHRESHOLD (1.5F)
+
+/* -------------------------------------------------------------------------------------------
+* Short Device Name --------------------------------------------------------------------------
+* --------------------------------------------------------------------------------------------
+* This project is named: "Open Virtual Steering", too long to be suitable for BLE device 
+* naming and advertising. A made-up short device name is default inserted.
+* However, users may need to configure the short device name manually for interoperability on 
+* certain platforms to allow for instant steering device recognition and full functionality.  
+* For general information on BLE naming conventions, 
+*                    refer to https://www.bluetooth.com/specifications/specs/ 
+* -------------------------------------------------------------------------------------------*/
+#define SHORTDEVICENAME "STERZOPN"  // String length is max 8
+// -------------------------------------------------------------------------------------------
+
+class ServerCallbacks : public BLEServerCallbacks {
+    void onConnect(BLEServer* pServer, esp_ble_gatts_cb_param_t *param) {
+#ifdef DEBUG
+      // Get some connection parameters of the peer device.
+      uint8_t address[6];
+      memcpy(&address, param->connect.remote_bda, 6);
+      DEBUG_PRINT("Server connected to Client with MAC Address: ");
+      BLESteeringServer::getInstance().printPeerAddress(address);
+#endif
+      BLEDevice::stopAdvertising();
+      BLESteeringServer::getInstance().isConnected = true;
+    };
+
+    void onDisconnect(BLEServer* pServer, esp_ble_gatts_cb_param_t *param) {
+#ifdef DEBUG
+      // Get some connection parameters of the peer device.
+      uint8_t address[6];
+      memcpy(&address, param->connect.remote_bda, 6);
+      DEBUG_PRINT("Server disconnected from Client with MAC Address: ");
+      BLESteeringServer::getInstance().printPeerAddress(address);
+#endif
+      BLESteeringServer::getInstance().isConnected = false;
+      BLEDevice::startAdvertising();
+    }
+};
+
+class pRxCharCallbacks : public BLECharacteristicCallbacks {
+  void onWrite(BLECharacteristic* pCharacteristic) {
+        String rxDataString = pCharacteristic->getValue();
+        uint8_t RxDataLen = rxDataString.length();    // Get the actual length of data bytes
+        uint8_t RxData[RxDataLen];
+        memset(RxData, 0, RxDataLen); // set to zero
+        // Display the raw request packet actual length
+        DEBUG_PRINTF("pRxChar (31) [Len: %d] [Data: ", RxDataLen);
+        // Transfer the contents of data to RxData for further processing!
+        for (int i = 0; i < RxDataLen; i++) {
+            RxData[i] = rxDataString[i];
+            // Display the raw request packet byte by byte in HEX
+            DEBUG_PRINTF("%02X ", RxData[i], HEX);
+        }
+        DEBUG_PRINTLN("]"); 
+  } // onWrite
+}; // pRxCharCallbacks 
+
+class SteeringChar2902Callbacks : public BLEDescriptorCallbacks {
+  void onWrite(BLEDescriptor* pDescriptor) {
+    uint8_t *value = pDescriptor->getValue();
+    if(value[0] == 0) {
+      DEBUG_PRINTLN("Central Notify Disabled SteeringChar (30)");
+      BLESteeringServer::getInstance().pSteeringChar_Notify_Enabled = false;
+    } else {
+      DEBUG_PRINTLN("Central Notify Enabled SteeringChar (30)");
+      BLESteeringServer::getInstance().pSteeringChar_Notify_Enabled = true;
+    }
+  }
+};
+
+class TxChar2902Callbacks : public BLEDescriptorCallbacks {
+  void onWrite(BLEDescriptor* pDescriptor) {
+    uint8_t *value = pDescriptor->getValue();
+    if(value[0] == 0) {
+      DEBUG_PRINTLN("Central Indicate Disabled TxChar (32)");
+      BLESteeringServer::getInstance().pTxChar_Indicate_Enabled = false;
+    } else {
+      DEBUG_PRINTLN("Central Indicate Enabled TxChar (32)");
+      BLESteeringServer::getInstance().pTxChar_Indicate_Enabled = true;
+    }
+  }
+};
+
+class BatteryChar2902Callbacks : public BLEDescriptorCallbacks {
+  void onWrite(BLEDescriptor* pDescriptor) {
+    uint8_t *value = pDescriptor->getValue();
+    if(value[0] == 0) {
+      DEBUG_PRINTLN("Central Notify Disabled BatteryChar (0x2A19)");
+      BLESteeringServer::getInstance().pBatteryChar_Notify_Enabled = false;
+    } else {
+      DEBUG_PRINTLN("Central Notify Enabled BatteryChar (0x2A19)");
+      BLESteeringServer::getInstance().pBatteryChar_Notify_Enabled = true;
+      BLESteeringServer::getInstance().updateBatteryPercentage((uint8_t)100); // Notify pBatteryChar first time 
+    }
+  }
+};
+
+// BLESteeringServer Class Members----------------------------------------------------------------------------------------
+
+BLESteeringServer& BLESteeringServer::getInstance() {
+    static BLESteeringServer instance;
+    return instance;
+}
+
+void BLESteeringServer::begin(void) {
+    BLEDevice::init(SHORTDEVICENAME);
+    DEBUG_PRINTLN("Creating Bluedroid Server...");
+    pServer = BLEDevice::createServer();
+    pServer->setCallbacks(new ServerCallbacks());
+    // Create the Steering Device Service (01)
+    DEBUG_PRINTLN("Define Steering Device Service...");
+    pService = pServer->createService(STEERING_DEVICE_UUID);
+    // Create Steering Device Characteristics (30)
+    pSteeringChar = pService->createCharacteristic(STEERING_ANGLE_CHAR_UUID, BLECharacteristic::PROPERTY_NOTIFY);
+    pSteeringChar2902 = new BLE2902();
+    pSteeringChar2902->setCallbacks(new SteeringChar2902Callbacks()); 
+    pSteeringChar->addDescriptor(pSteeringChar2902);
+    pSteeringChar->setValue(defaultValue, 4); // Set initial value
+    // Create pTxChar Device Characteristics (32)
+    pTxChar = pService->createCharacteristic(STEERING_TX_CHAR_UUID, BLECharacteristic::PROPERTY_INDICATE); 
+    pTxChar2902 = new BLE2902();
+    pTxChar2902->setCallbacks(new TxChar2902Callbacks()); 
+    pTxChar->addDescriptor(pTxChar2902);
+    pTxChar->setValue(defaultValuepTxChar, 4); // Set initial value
+    // Create pRxChar Device Characteristics (31)
+    pRxChar = pService->createCharacteristic(STEERING_RX_CHAR_UUID, BLECharacteristic::PROPERTY_WRITE);
+    pRxChar->setCallbacks(new pRxCharCallbacks());
+    pRxChar->setValue(defaultValue, 4); // Set initial value
+    // Start the Steering Device service (01)
+    pService->start();
+
+    // Create the Battery Service
+    DEBUG_PRINTLN("Define Battery Service...");
+    pBatteryService = pServer->createService(UUID16_SVC_BATTERY );
+    pBatteryChar = pBatteryService->createCharacteristic(UUID16_CHR_BATTERY_LEVEL, BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_NOTIFY);
+    pBatteryChar2902 = new BLE2902();
+    pBatteryChar2902->setCallbacks(new BatteryChar2902Callbacks()); 
+    pBatteryChar->addDescriptor(pBatteryChar2902);
+    pBatteryChar->setValue(&defaultBatteryLevel, 1); // Default 100% battery
+    pBatteryService->start();
+
+    // Setup Advertising
+    BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+    pAdvertising->addServiceUUID(STEERING_DEVICE_UUID);
+    BLEDevice::startAdvertising();
+    DEBUG_PRINTLN("Start Advertising...");  
+}
+
+void BLESteeringServer::printPeerAddress(uint8_t addr[6]) {
+    for (int i = 1; i < 6; i++) {
+      // Display byte by byte in HEX reverse: little Endian
+      DEBUG_PRINTF("%02X:",addr[(6-i)], HEX);
+    }
+   DEBUG_PRINTF("%02X\n",addr[0], HEX);
+}
+
+float BLESteeringServer::constrainSteerAngle(float angle) {
+    if (fabs(angle) > MAX_STEER_ANGLE) { 
+      if (angle > 0) { angle = MAX_STEER_ANGLE; }
+      else { angle = - MAX_STEER_ANGLE; }
+    }
+    if (fabs(angle) < STEERANGLETHRESHOLD) { angle= 0.0; }
+    return angle;
+}
+
+bool BLESteeringServer::updateSteeringValue(float angle) {
+    if (pSteeringChar_Notify_Enabled) {
+        float steerAngle = constrainSteerAngle(angle);
+        pSteeringChar->setValue((uint8_t*)&steerAngle, 4); // Float stored in array of 4 bytes
+        pSteeringChar->notify();
+        return true;
+    }
+    return false;
+}
+bool BLESteeringServer::updateBatteryPercentage(uint8_t batteryPercentage) {
+    if (pBatteryChar_Notify_Enabled) {
+        pBatteryChar->setValue(&batteryPercentage, 1); // uint8_t stored in array of 1 byte
+        pBatteryChar->notify(); 
+        DEBUG_PRINTF("Updated Battery Level: %3d%%\n", batteryPercentage);
+        return true;
+    }
+    return false;
+}
+
+bool BLESteeringServer::sendResponse(uint8_t *data, size_t size) {
+    if (pTxChar_Indicate_Enabled) {
+        pTxChar->setValue(data, size);
+        pTxChar->indicate(); 
+        return true;
+    }
+    return false;
+}
+
+
---
+++ src/BLESteeringServer.h
@@ -0,0 +1,53 @@
+#ifndef BLE STEERING_SERVER_H
+#define BLE STEERING_SERVER_H
+
+#include <arduino.h>
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
+#include <BLEAdvertising.h>
+#include <BLE2902.h>
+
+class BLESteeringServer : public BLEServerCallbacks, public BLECharacteristicCallbacks, public BLEDescriptorCallbacks {
+public:
+    static BLESteeringServer& getInstance();
+
+    void begin(void);
+    bool updateSteeringValue(float angle);
+    bool updateBatteryPercentage(uint8_t batteryPercentage);
+    bool sendResponse(uint8_t *data, size_t size);
+    bool pSteeringChar_Notify_Enabled = false;
+    bool pTxChar_Indicate_Enabled = false; 
+    bool pBatteryChar_Notify_Enabled = false; 
+    bool isConnected = false;
+
+    void printPeerAddress(uint8_t addr[6]);
+
+private:
+    // Singleton
+    BLESteeringServer() = default;
+    BLESteeringServer(const BLESteeringServer&) = delete;
+    BLESteeringServer& operator=(const BLESteeringServer&) = delete;
+
+    // BLE internal objects
+    BLEServer *pServer = nullptr;
+    BLEService *pService = nullptr;
+    BLECharacteristic *pSteeringChar = nullptr;
+    BLE2902 *pSteeringChar2902 = nullptr;
+    BLECharacteristic *pRxChar = nullptr;
+    BLECharacteristic *pTxChar = nullptr;
+    BLE2902 *pTxChar2902 = nullptr;
+    BLEService *pBatteryService = nullptr;
+    BLECharacteristic *pBatteryChar = nullptr;
+    BLE2902 *pBatteryChar2902 = nullptr;
+
+    float constrainSteerAngle(float angle);
+
+    //  Presets 
+    uint8_t defaultValue[4] = {0x0,0x0,0x0,0x0};                    // Default 4 byte Value zero
+    uint8_t defaultValuepTxChar[4] = {0x03,0x10,0x12,0x34};         // Default 4 byte value
+    uint8_t defaultBatteryLevel = 100;
+
+};
+
+#endif // BLE STEERING_SERVER_H
